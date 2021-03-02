708a163a44602b5048e4741158bd6753d4ae1352
cmarius46 <cmarius46@gmail.com>
2021-03-02 15:47:09 +0200

    async failed successfully

--- src/main.cpp
+++ src/main.cpp
@@ -3,64 +3,104 @@
 
 using namespace std;
 
-struct Neuron {
 
+void show(int a) {
+	cout << a << "async !" << '\n';
+}
+
+struct Neuron {
 	int pos;
 	vector<string> data;
 
 	vector<Neuron*> next;
 	vector<Neuron*> prev;
 
-	Neuron() {
-		this->pos = -1;
-		this->data = vector<string>();
-	}
+	unordered_map<int, Neuron*> mappedNext;
+	unordered_map<int, Neuron*> mappedPrev;
 
-	Neuron(int p, vector<string> v) {
-		// fiecarui neuron ii este atribuita o pozitie si sunt
-		// recapatate datele din memorie
-
-		this->pos = p;
-		this->data = v;
-	}
-
-	void addPrev(Neuron *n) {
-		// adds a neuron n as a predecessor of the current neuron
-		this->prev.push_back(n);
-		//TODO check if it already exists
-
-		//if(!exists)
-		n->addNext(this);
-	}
-
-	void addNext(Neuron *n) {
-		// adds a neuron n as a successor of the current neuron
-		this->next.push_back(n);
-		//TODO check if it already exists
-	}
-
+	Neuron();
+	Neuron(int , vector<string> );
+	void addPrev(int , Neuron* );
+	void addNext(int , Neuron* );
 	void process();
-	// self function to process data;
+	void send();
+	void receive(vector<string>);
+	void print();
 
-	void send(){}
-
-	// bool send(old) returns true if sent the message to at least another neuron
-
-	void receive();
-
-	// bool receive(old) returns true if received at least one message 
-
-	void print() {
-		// debugs the data of the neuron
-
-		cout << pos << " ";
-		for(auto word : data) {
-			cout << word << " ";
-		}
-		cout << "\n\n";
-	}
 
 };
+
+Neuron::Neuron() {
+	this->pos = -1;
+	this->data = vector<string>();
+}
+
+Neuron::Neuron(int p, vector<string> v) {
+	// fiecarui neuron ii este atribuita o pozitie si sunt
+	// recapatate datele din memorie
+
+	this->pos = p;
+	this->data = v;
+}
+
+void Neuron::addPrev(int pos, Neuron *pn) {
+
+	// adds a neuron n as a predecessor of the current neuron
+	// pos is the position of n, pn is a pointer to n
+
+	this->prev.push_back(pn);
+	mappedPrev[pos] = pn;
+	//TODO check if it already exists
+
+	//if(!exists)
+	pn->addNext(this->pos, this);
+}
+
+void Neuron::addNext(int pos, Neuron *pn) {
+	// adds a neuron n as a successor of the current neuron
+	// pos is the position of n, pn is a pointer to n
+
+	this->next.push_back(pn);
+	mappedNext[pos] = pn;
+	//TODO check if it already exists
+}
+
+void Neuron::process() {
+
+}
+// self function to process data;
+
+void Neuron::send() {
+
+	//future<void> var = async(mappedNext[2]->receive, this->data);
+	// future<void> var = async(show, 17);
+
+	mappedNext[2]->receive(this->data);
+	// TODO should be done asynchronous
+
+	cout << "sent" << '\n';
+}
+
+// bool send(old) returns true if sent the message to at least another neuron
+
+void Neuron::receive(vector<string> v) {
+	this->data = v;
+	cout << "received" << '\n';
+}
+
+// bool receive(old) returns true if received at least one message 
+
+void Neuron::print() {
+	// debugs the data of the neuron
+
+	cout << pos << " ";
+	for(auto word : data) {
+		cout << word << " ";
+	}
+	cout << "\n\n";
+}
+
+
 
 struct Memory {
 
@@ -93,10 +133,11 @@ int main() {
 	Neuron n2(2, memory.mem[2]);
 	n2.print();
 
-	n2.addPrev(&n1);
+	n2.addPrev(1, &n1);
 
 	n1.send();
 	n2.print();
+	n1.next[0]->print();
 
 	return 0;
 }
